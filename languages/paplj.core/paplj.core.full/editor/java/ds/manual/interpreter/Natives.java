cccf8892a2affb26c1c3081d0db68a2965f01552
Vlad Vergu <v.vergu@gmail.com>
2015-11-23 15:44:09 +0100

    Add missing dependency on the interpreter framework. Add dummy Natives implementation

---
+++ languages/paplj.core/paplj.core.full/editor/java/ds/manual/interpreter/Natives.java
@@ -0,0 +1,25 @@
+package ds.manual.interpreter;
+
+public class Natives {
+
+	public static int plusI_2(int i13000000, int j9000000) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static boolean eqI_2(int i12000000, int j8000000) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static int fresh() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static int str2int_1(String i8000000) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
