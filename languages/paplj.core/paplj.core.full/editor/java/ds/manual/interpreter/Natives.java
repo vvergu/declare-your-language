bc2662844e46a5bbd945091b426ad18487dc70d6
Vlad Vergu <v.vergu@gmail.com>
2015-11-25 12:55:46 +0100

    add dummy natives

--- languages/paplj.core/paplj.core.full/editor/java/ds/manual/interpreter/Natives.java
+++ languages/paplj.core/paplj.core.full/editor/java/ds/manual/interpreter/Natives.java
@@ -22,4 +22,19 @@ public class Natives {
 		return 0;
 	}
 
+	public static int minusI_2(int i7000000, int j5000000) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static boolean gtI_2(int j7000000, int i11000000) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static int timesI_2(int i9000000, int j6000000) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
